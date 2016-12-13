c37191e19e9d415107ac777a4209538ad9c0358c
lambor <1973124271@qq.com>
2016-12-13 11:42:33 +0800

    complete day10

---
+++ day_10/kernel/main.c
@@ -0,0 +1,125 @@
+#include "header.h"
+
+
+int bootmain()
+{
+	//buf the string.
+	char s[50] = {0};
+	
+	init_gdtidt();
+	init_pic();
+	io_sti();
+
+	init_keyboard();
+	struct MOUSE_DEC mdec = {0};
+	enable_mouse(&mdec);
+
+	//init memory manager
+	unsigned int memtotal;
+	struct MEMMAN *memman  = (struct MEMMAN *)MEMMAN_ADDR;
+	memtotal = memtest(0x00400000,0xbfffffff);
+	memman_init(memman);
+	//init memman with two mem block:0x00001000-0x0009efff and 0x00400000-memtotal
+	memman_free(memman,0x00001000,0x0009e000);
+	memman_free(memman,0x00400000,memtotal-0x00400000);
+
+	//entry.S write the boot info into 0x0ff0
+	struct BOOTINFO *binfo = (struct BOOTINFO *)0xff0;
+	
+	init_palette();
+	//sheet control
+	struct SHTCTL *shtctl;
+	struct SHEET *sht_bg, *sht_mouse;
+	unsigned char *buf_bg,*buf_mouse[256];
+	
+	shtctl = shtctl_init(memman,binfo->vram,binfo->scrnx,binfo->scrny);
+	sht_bg = sheet_alloc(shtctl);
+	sht_mouse = sheet_alloc(shtctl);
+	//buf_bg = (unsigned char *)memman_alloc_4k(memman,binfo->scrnx*binfo->scrny);
+	buf_bg = (unsigned char *)memman_alloc_4k(memman,TRY_BG_SIZE);
+	sheet_setbuf(sht_bg,buf_bg,binfo->scrnx,binfo->scrny,-1);
+	sheet_setbuf(sht_mouse,buf_mouse,16,16,99);
+	
+	test(binfo->scrnx,binfo->scrny);
+	//init screen and mouse sheet
+	init_screen8(buf_bg,binfo->scrnx,binfo->scrny);
+	init_mouse_cursor8(buf_mouse,99);
+	sheet_slide(shtctl,sht_bg,0,0);
+	int mx = (binfo->scrnx-16)/2;
+	int my = (binfo->scrny-28-16)/2;
+	sheet_slide(shtctl,sht_mouse,mx,my);
+	sheet_updown(shtctl,sht_bg,0);
+	sheet_updown(shtctl,sht_mouse,1);
+	
+	putfonts8_asc(buf_bg,binfo->scrnx,8,8,COL8_FFFFFF,"ABC 123");
+	putfonts8_asc(buf_bg,binfo->scrnx,31,31,COL8_000000,"dcnh os.");
+	putfonts8_asc(buf_bg,binfo->scrnx,30,30,COL8_FFFFFF,"dcnh os.");
+	
+	//test memory size
+	sprintf(s,"memory %dMB, free %dKB.",memtotal/(1024*1024),memman_total(memman)/1024);
+	putfonts8_asc(buf_bg,binfo->scrnx,0,48,COL8_FFFFFF,s);
+	sheet_refresh(shtctl,sht_bg,0,0,binfo->scrnx,64);
+	
+	//pic enable
+	io_out8(PIC0_IMR,0xf9);
+	io_out8(PIC1_IMR,0xef);
+
+	//keyboard mouse fifo
+	extern struct FIFO8 keybuf,mousebuf;
+	char _keybuf[32] = {0};
+	char _mousebuf[128] = {0};
+ 
+	fifo8_init(&keybuf,32,_keybuf);
+	fifo8_init(&mousebuf,128,_mousebuf);
+	for(;;)
+	{
+		io_cli();
+		if(fifo8_status(&keybuf) + fifo8_status(&mousebuf) == 0)
+		{
+			io_stihlt();
+		}
+		else
+		{
+			unsigned char i;
+			if(fifo8_status(&keybuf))
+			{
+				i = fifo8_get(&keybuf);
+				io_sti();
+				xtoa(i,s);
+				boxfill8(buf_bg,binfo->scrnx,COL8_000000,0,16,31,31);
+				putfonts8_asc(buf_bg,binfo->scrnx,0,16,COL8_FFFFFF,s);
+				sheet_refresh(shtctl,sht_bg,0,16,31,31);
+			}
+			else if(fifo8_status(&mousebuf))
+			{
+				i = fifo8_get(&mousebuf);
+				io_sti();
+				if(mouse_decode(&mdec,i)!=0)
+				{
+					sprintf(s,"[lcr %d %d]",mdec.x,mdec.y);
+					if(mdec.btn & 0x01)
+						s[1] = 'L';
+					if(mdec.btn & 0x02)
+						s[3] = 'R';
+					if(mdec.btn & 0x04)
+						s[2] = 'C';
+					boxfill8(buf_bg,binfo->scrnx,COL8_008484,32,16,32+15*8-1,31);
+					putfonts8_asc(buf_bg,binfo->scrnx,32,16,COL8_FFFFFF,s);
+					
+					mx += mdec.x;
+					my += mdec.y;
+					if(mx<0) mx=0;
+					if(my<0) my=0;
+					if(mx>binfo->scrnx - 16) mx=binfo->scrnx-16;
+					if(my>binfo->scrny - 16) my=binfo->scrny-16;
+					sprintf(s,"(%d,%d)",mx,my);
+					boxfill8(buf_bg,binfo->scrnx,COL8_008484,0,0,79,15);
+					putfonts8_asc(buf_bg,binfo->scrnx,0,0,COL8_FFFFFF,s);
+					sheet_refresh(shtctl,sht_bg,0,0,151,31);
+					sheet_slide(shtctl,sht_mouse,mx,my);
+				}
+			}
+			
+		}
+	}
+}
---
+++ day_10/kernel/sheet.c
@@ -0,0 +1,221 @@
+#include "sheet.h"
+
+struct SHTCTL* shtctl_init(struct MEMMAN *man,unsigned char *vram,int xsize,int ysize)
+{
+	struct SHTCTL *ctl;
+	int i;
+	ctl = (struct SHTCTL *)memman_alloc_4k(man,sizeof(struct SHTCTL));
+	if(ctl == 0)
+		goto err;
+	ctl->vram = vram;
+	ctl->xsize= xsize;
+	ctl->ysize= ysize;
+	ctl->top = -1;
+	for(i=0;i<MAX_SHEETS;i++)
+		ctl->sheets0[i].flags = 0;
+
+	err:
+		return ctl;
+}
+
+struct SHEET *sheet_alloc(struct SHTCTL *ctl)
+{
+	struct SHEET *sht;
+	int i;
+	for(i=0;i<MAX_SHEETS;i++)
+	{
+		if(ctl->sheets0[i].flags == 0)
+		{
+			sht = &ctl->sheets0[i];
+			sht->flags = SHEET_USE;
+			sht->height = -1;
+			return sht;
+		}
+	}
+	return 0;
+}
+
+void sheet_setbuf(struct SHEET *sht,unsigned char *buf,int xsize,int ysize,int col_inv)
+{
+	sht->buf = buf;
+	sht->bxsize = xsize;
+	sht->bysize = ysize;
+	sht->col_inv = col_inv;
+	return;
+}
+
+//update the sheet with new height
+void sheet_updown(struct SHTCTL *ctl,struct SHEET *sht,int height)
+{
+	int h,old = sht->height;
+	if(height>ctl->top+1) height = ctl->top + 1;
+	if(height<-1) height = -1;
+	sht->height = height;
+	
+	//push down the sheet
+	if(old>height)
+	{
+		//if the sheet still shows
+		if(height >=0 )
+		{
+			for(h=old;h>height;h--)
+			{
+				ctl->sheets[h] = ctl->sheets[h-1];
+				ctl->sheets[h]->height = h;
+			}
+			ctl->sheets[height] = sht;	
+		}
+		else //the sheet hides from showing
+		{
+			for(h=old;h<ctl->top;h++)
+			{
+				ctl->sheets[h] = ctl->sheets[h+1];
+				ctl->sheets[h]->height = h;
+			}
+			ctl->top--;
+		}
+		//sheet_refresh(ctl);
+		sheet_refresh(ctl,sht,0,0,sht->bxsize,sht->bysize);
+	}
+	else if(old<height) //pop up the sheet
+	{
+		if(old >= 0) //the sheet still shows.
+		{
+			for(h=old;h<height;h++)
+			{
+				ctl->sheets[h] = ctl->sheets[h+1];
+				ctl->sheets[h]->height = h;
+			}
+			ctl->sheets[height] = sht;
+		}
+		else //the sheet shows from hiding.
+		{
+			ctl->top++;
+			for(h=ctl->top;h>height;h--)
+			{
+				ctl->sheets[h] = ctl->sheets[h-1];
+				ctl->sheets[h]->height = h;
+			}
+			ctl->sheets[height] = sht;
+		}
+		//sheet_refresh(ctl);
+		sheet_refresh(ctl,sht,0,0,sht->bxsize,sht->bysize);
+	}
+	//else height not change.
+	return;
+}
+
+//not good refresh
+//obsolete
+void sheet_refresh_ob(struct SHTCTL *ctl)
+{
+	int h,bx,by,vx,vy;
+	unsigned char *buf,c,*vram=ctl->vram;
+	struct SHEET *sht;
+	for(h=0;h<=ctl->top;h++)
+	{
+		sht = ctl->sheets[h];
+		buf = sht->buf;
+		for(by=0;by<sht->bysize;by++)
+		{
+			vy = sht->vy0+by;
+			for(bx=0;bx<sht->bxsize;bx++)
+			{
+				vx = sht->vx0+bx;
+				c=buf[by*sht->bxsize+bx];
+				if(c!=sht->col_inv)
+					vram[vy*ctl->xsize+vx] = c;
+			}
+		}
+	}
+	return;
+}
+
+
+
+void sheet_refreshsub1(struct SHTCTL *ctl,int vx0,int vy0,int vx1,int vy1)
+{
+	int h,bx,by,vx,vy;
+	unsigned char *buf,c,*vram = ctl->vram;
+	struct SHEET *sht;
+	for(h=0;h<=ctl->top;h++)
+	{
+		sht = ctl->sheets[h];
+		buf = sht->buf;
+		for(by=0;by<sht->bysize;by++)
+		{
+			vy = sht->vy0 + by;
+			for(bx=0;bx<sht->bxsize;bx++)
+			{
+				vx = sht->vx0 + bx;
+				if(vx0<=vx && vx<vx1 && vy0<=vy && vy<vy1)
+				{
+					c = buf[by*sht->bxsize+bx];
+					if(c!=sht->col_inv)
+						vram[vy*ctl->xsize+vx] = c;
+				}
+			}
+		}
+	}
+}
+
+void sheet_refreshsub2(struct SHTCTL *ctl,int vx0,int vy0,int vx1,int vy1)
+{
+	int h,bx,by,vx,vy,bx0,by0,bx1,by1;
+	unsigned char *buf,c,*vram = ctl->vram;
+	struct SHEET *sht;
+	for(h=0;h<=ctl->top;h++)
+	{
+		sht = ctl->sheets[h];
+		buf = sht->buf;
+		bx0 = vx0 - sht->vx0;
+		by0 = vy0 - sht->vy0;
+		bx1 = vx1 - sht->vx0;
+		by1 = vy1 - sht->vy0;
+
+		if(bx0<0) bx0=0;
+		if(by0<0) by0=0;
+		if(bx1>sht->bxsize) bx1=sht->bxsize;
+		if(by1>sht->bysize) by1=sht->bysize;
+		for(by=by0;by<by1;by++)
+		{
+			vy = sht->vy0+by;
+			for(bx=bx0;bx<bx1;bx++)
+			{
+				vx = sht->vx0+bx;
+				c  = buf[by*sht->bxsize+bx];
+				if(c!=sht->col_inv)
+					vram[vy*ctl->xsize+vx] = c;
+			}
+		}
+	}
+}
+
+void sheet_refresh(struct SHTCTL *ctl,struct SHEET *sht,int bx0,int by0,int bx1,int by1)
+{
+	if(sht->height >= 0)
+		sheet_refreshsub2(ctl,sht->vx0+bx0,sht->vy0+by0,sht->vx0+bx1,sht->vy0+by1);
+	return;
+}
+
+void sheet_slide(struct SHTCTL *ctl, struct SHEET *sht,int vx0,int vy0)
+{
+	//(vx0,vy0): the sheet buf in the vram pos.
+	int old_vx0 = sht->vx0,old_vy0 = sht->vy0;
+	sht->vx0 = vx0;
+	sht->vy0 = vy0;
+	if(sht->height >= 0)
+	{
+		sheet_refreshsub2(ctl,old_vx0,old_vy0,old_vx0+sht->bxsize,old_vy0+sht->bysize);
+		sheet_refreshsub2(ctl,vx0,vy0,vx0+sht->bxsize,vy0+sht->bysize);
+	}
+	return;
+}
+
+void sheet_free(struct SHTCTL *ctl, struct SHEET *sht)
+{
+	if(sht->height >= 0)
+		sheet_updown(ctl,sht,-1); //hide the sheet
+	sht->flags = 0; //set sheet unused.
+	return;
+}
